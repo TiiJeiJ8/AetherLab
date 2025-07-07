3d1e9d159eb64f04c64bc59575e4ff5e906c5488
TiiJeiJ8 <981504692@qq.com>
2025-07-07 15:49:36 +0800

    Refactor App.vue layout and move ThemeButton
    Wrapped the main content in an 'app-root' div and moved the ThemeButton component outside the inner container for improved layout structure.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="min-vh-100 bg-color position-relative">
-    <router-view></router-view>
+  <div class="app-root">
+    <div class="min-vh-100 bg-color position-relative">
+      <router-view></router-view>
+    </div>
+    <ThemeButton />
   </div>
-  <ThemeButton />
 </template>
 
 <script>
