46613f010b5596c4f71565e7f97a82a0b38d665d
TiiJeiJ8 <981504692@qq.com>
2025-09-23 23:21:16 +0800

    Add data quality report component and utility
    Introduced dataQualityReport.vue for displaying data quality reports and added a placeholder generateDataQualityReport function in reportGen.js. These additions set up the structure for future implementation of data quality reporting features.

---
+++ frontend/src/assets/JS/utils/reportGen.js
@@ -0,0 +1,7 @@
+// reportGen.js
+
+/* eslint-disable */
+
+// 数据质量报告生成函数
+export function generateDataQualityReport(fileDataMap, fileName) {
+}
---
+++ frontend/src/components/Preprocessing/module/dataQualityReport.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="data-quality-report-container">
+    <div v-if="!report" class="report-placeholder">
+        <p style="user-select: none;">Generating data quality report...</p>
+    </div>
+    <div v-else class="report-content">
+    </div>
+</div>
+</template>
+
+<script setup>
+/* eslint-disable */
+import { ref } from 'vue'
+import { workspaceFiles, fileDataMap } from '@/assets/JS/utils/dataStructureOptimize.js'
+
+import { generateDataQualityReport } from '@/assets/JS/utils/reportGen.js'
+const report = ref(null)
+
+</script>
+
+<style scoped>
+.data-quality-report-container {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    overflow-y: auto;
+}
+.report-placeholder {
+    text-align: center;
+    color: #888;
+    font-size: 1.2em;
+}
+</style>
